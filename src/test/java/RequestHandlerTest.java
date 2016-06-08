aef5a715e488043cf0c90b8a19f08a7f82f01d00
Tyler Decker <tyler.decker@gmail.com>
2016-06-08 11:47:40 -0500

    Added mockito and Mocks for testing.

--- src/test/java/RequestHandlerTest.java
+++ src/test/java/RequestHandlerTest.java
@@ -1,19 +1,23 @@
 import com.td.HTTPServer.*;
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
 
 public class RequestHandlerTest extends junit.framework.TestCase {
     RequestHandler handler;
-    RequestVerifier realVerifier;
+    HTTPVerifierInterface verifier;
 
   protected void setUp() {
-    realVerifier = new RequestVerifier();
-    handler = new RequestHandler(realVerifier);
+    verifier = mock(HTTPVerifierInterface.class);
+    handler = new RequestHandler(verifier);
   }
 
   public void testValidRequest() {
+    when(verifier.isValidHTTP(anyString())).thenReturn(true);
     assertEquals("200", handler.processRequest("GET / HTTP/1.1"));
   }
 
   public void testInvalidRequest() {
+    when(verifier.isValidHTTP(anyString())).thenReturn(false);
     assertEquals("400", handler.processRequest("jkljd"));
   }
 }
