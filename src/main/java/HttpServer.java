43ea365f9a0f680bbce216a6fcc50a52a62c46d6
tyler <tyler.decker@gmail.com>
2016-06-17 09:26:11 -0500

    Added abliity to pass portNumber as cmd line args.
    Updated the gradle build file to send command line arguments to the
    application during run task.
    
    Updated HttpServer to look for argument -pn and then set the portNumber
    to the next argument after that.

--- src/main/java/HttpServer.java
+++ src/main/java/HttpServer.java
@@ -4,8 +4,16 @@ import java.io.IOException;
 public class HttpServer{
 
   public static void main(String[] args) throws IOException {
-    System.out.println("HTTP Server running on localhost port 8080!!!");
-    HttpReaderWriter httpReaderWriter = new HttpReaderWriter(new SocketIO(8080), new HttpVerifier());
+    int portNumber = 8080;
+    for (int i = 0; i < args.length; i++) {
+      System.out.println(args[i]);
+      if (args[i].equals("-pn")) {
+        portNumber = Integer.parseInt(args[++i]);
+      }
+    }
+
+    System.out.println("HTTP Server running on localhost port " + portNumber +"!");
+    HttpReaderWriter httpReaderWriter = new HttpReaderWriter(new SocketIO(portNumber), new HttpVerifier());
     HttpServerRunner httpServerRunner = new HttpServerRunner(httpReaderWriter, new HttpHandlerSelector());
     httpServerRunner.runServer();
   }
