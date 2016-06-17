10d76a685cb8b9f957eb282daff0cf85b95dd6d0
tyler <tyler.decker@gmail.com>
2016-06-17 09:32:45 -0500

    Added error message to 404 response.

--- src/main/java/HttpHandlerSelector.java
+++ src/main/java/HttpHandlerSelector.java
@@ -9,7 +9,7 @@ public class HttpHandlerSelector {
   FileIO fileIO;
 
   public HttpResponse generateResponse(HttpRequest request) {
-    if (request.method().contains("GET")) {
+    if (request.method().equals("GET")) {
       return getRequest(request);
     }
     return new HttpResponse("HTTP/1.1 200 OK");
@@ -26,9 +26,13 @@ public class HttpHandlerSelector {
   private HttpResponse getRequestTextFile(HttpRequest request) {
     fileIO = new FileIO("./site/textFiles");
     String contents = fileIO.getFileContents(request.path());
-   StringBuilder strBuilder = new StringBuilder();
-   strBuilder.append("HTTP/1.1 200 OK\n\r\n\r");
-   strBuilder.append(contents);
-   return new HttpResponse(strBuilder.toString());
+    StringBuilder strBuilder = new StringBuilder();
+    if (contents.contains("Error")) {
+      strBuilder.append("HTTP/1.1 404 File or Directory not found\n\r\n\r");
+    } else {
+      strBuilder.append("HTTP/1.1 200 OK\n\r\n\r");
+    }
+    strBuilder.append(contents);
+    return new HttpResponse(strBuilder.toString());
   }
 }
