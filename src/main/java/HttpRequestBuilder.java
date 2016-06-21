95a65a6871687892c572178ec32dc12249bae764
Tyler Decker <tyler.decker@gmail.com>
2016-06-21 10:55:08 -0500

    Recaftored to make code easier to read.

--- src/main/java/HttpRequestBuilder.java
+++ src/main/java/HttpRequestBuilder.java
@@ -10,26 +10,19 @@ public class HttpRequestBuilder implements IRequestBuilder {
   }
 
   public HttpRequest createRequest(String rawRequest) throws InvalidHttpRequestException {
-    // TODO clean this up!
     String rawRequestArray[] = rawRequest.split("\n", 2);
-    String rawHeader;
     String requestLine = rawRequest.split("\n")[0];
     if (!requestVerifier.isValid(requestLine)) {
        throw new InvalidHttpRequestException();
     }
-    HashMap<String, String> headers;
-    if (rawRequestArray.length < 2) {
-      headers = new HashMap<String, String>();
-    } else {
-      rawHeader = rawRequest.split("\n", 2)[1];
-      headers = buildHeaderMap(rawHeader);
-    }
+    HashMap<String, String> headers = buildHeaderMap(rawRequest);
     return new HttpRequest(requestLine, headers);
   }
 
-  private HashMap<String, String> buildHeaderMap(String rawHeader) {
+  private HashMap<String, String> buildHeaderMap(String rawRequest) {
     HashMap<String, String> rtnMap = new HashMap<String, String>();
     try {
+      String rawHeader = rawRequest.split("\n", 2)[1];
       String rawHeaderLines[] = rawHeader.split("\n");
       for (int i = 0; i < rawHeaderLines.length; i++) {
         String headerKeyValue[] = rawHeaderLines[i].split(":", 2);
