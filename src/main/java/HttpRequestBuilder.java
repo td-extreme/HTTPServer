93b522eedab09615445d4cb6865386f927fad9d3
Tyler Decker <tyler.decker@gmail.com>
2016-06-21 12:50:43 -0500

    Removed catch IndexOutOfBoundsException.
    The code now checks to see if the array is the correct size before
    trying to access an index.
    
    Broke the buildHeaderMap method up, by adding two helper functions.
    
    One to parse the rawRequest into a String array that contains each line
    in the header.
    
    And antoher method that takes a String array and check each line to make
    sure it is in the formate key: value then addes that key value pair to
    the map. If it is not it thorws an InvalidHttpRequestException.

--- src/main/java/HttpRequestBuilder.java
+++ src/main/java/HttpRequestBuilder.java
@@ -13,23 +13,35 @@ public class HttpRequestBuilder implements IRequestBuilder {
     String rawRequestArray[] = rawRequest.split("\n", 2);
     String requestLine = rawRequest.split("\n")[0];
     if (!requestVerifier.isValid(requestLine)) {
-       throw new InvalidHttpRequestException();
+      throw new InvalidHttpRequestException();
     }
     HashMap<String, String> headers = buildHeaderMap(rawRequest);
     return new HttpRequest(requestLine, headers);
   }
 
-  private HashMap<String, String> buildHeaderMap(String rawRequest) {
-    HashMap<String, String> rtnMap = new HashMap<String, String>();
-    try {
-      String rawHeader = rawRequest.split("\n", 2)[1];
-      String rawHeaderLines[] = rawHeader.split("\n");
-      for (int i = 0; i < rawHeaderLines.length; i++) {
-        String headerKeyValue[] = rawHeaderLines[i].split(":", 2);
-        rtnMap.put(headerKeyValue[0], headerKeyValue[1]);
-      }
+  private HashMap<String, String> buildHeaderMap(String rawRequest) throws InvalidHttpRequestException {
+    String rawHeaderLines[] = parseHeader(rawRequest);
+    return createMap(rawHeaderLines);
+  }
+
+  private String[] parseHeader(String rawRequest) {
+    String[] rawRequestArray = rawRequest.split("\n", 2);
+    if (rawRequestArray.length < 2) {
+      return new String[] {};
     }
-    catch (ArrayIndexOutOfBoundsException e) { }
+    String rawHeader = rawRequestArray[1];
+    return rawHeader.split("\n");
+  }
+
+  private HashMap<String, String> createMap(String rawHeaderLines[]) throws InvalidHttpRequestException {
+    HashMap<String, String> rtnMap = new HashMap<String, String>();
+    for (int i = 0; i < rawHeaderLines.length; i++) {
+      String headerKeyValue[] = rawHeaderLines[i].split(":", 2);
+      if (headerKeyValue.length < 2) {
+        throw new InvalidHttpRequestException();
+      }
+      rtnMap.put(headerKeyValue[0], headerKeyValue[1]);
+    }
     return rtnMap;
   }
 }
