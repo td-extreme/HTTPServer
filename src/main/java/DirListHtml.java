35b47a6a9d42797ff5b500dd89643c0218e72bd3
Tyler Decker <tyler.decker@gmail.com>
2016-06-29 12:50:25 -0500

    Broke method up into smaller methods.

--- src/main/java/DirListHtml.java
+++ src/main/java/DirListHtml.java
@@ -5,21 +5,33 @@ import java.util.*;
 
 public class DirListHtml {
 
-  byte[] buildHtmlPage(String path, ArrayList<String> dirContents) {
+  public byte[] buildHtmlPage(String path, String[] dirContents) {
+    String rtnString = htmlHeader() + linksToFiles(path, dirContents) + htmlFooter();
+    return rtnString.getBytes();
+  }
+
+  private String linksToFiles(String path, String[] dirContents) {
     StringBuilder builder = new StringBuilder();
-    builder.append("<!DOCTYPE html>\n<html>\n<body>\n");
     for (String file: dirContents) {
-      builder.append("<a href=\"");
-      builder.append(path);
-      if ( !path.endsWith("/") ) {
-        builder.append("/");
-      }
-      builder.append(file);
-      builder.append("\">");
-      builder.append(file);
-      builder.append("</a><br>");
+      builder.append(htmlLink(path, file));
     }
-    builder.append("</body>\n</html>");
-    return builder.toString().getBytes();
+    return builder.toString();
+  }
+
+  private String htmlLink(String path, String file) {
+    String pathSlash = "";
+    if (!path.endsWith("/")) {
+      pathSlash = "/";
+    }
+    String rtnString = "<a href=\"" + path + pathSlash + file + "\">" + file + "</a><br>";
+    return rtnString;
+  }
+
+  private String htmlHeader() {
+    return "<!DOCTYPE html>\n\t<html>\n\t<body>\n";
+  }
+
+  private String htmlFooter() {
+    return "\n</body>\n</html>";
   }
 }
