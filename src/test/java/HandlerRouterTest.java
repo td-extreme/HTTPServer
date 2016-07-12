8dcf1f23500a81e6fceddf90fc00cbef379f1cac
Tyler Decker <tyler.decker@gmail.com>
2016-07-12 18:39:21 -0500

    Removed duplication.

--- src/test/java/HandlerRouterTest.java
+++ src/test/java/HandlerRouterTest.java
@@ -11,47 +11,47 @@ public class HandlerRouterTest extends junit.framework.TestCase {
   protected void setUp() {
     mockFileIO = new MockFileIO();
     handlerRouter = new HandlerRouter(mockFileIO);
-    request = new HttpRequest("GET /file.txt HTTP/1.1", new HashMap<String, String>());
   }
 
   public void testRouteForGetFile() {
+    buildRequest("GET /file.txt HTTP/1.1");
     mockFileIO.setIsFileTrue();
-    String type = getType();
-    assertEquals(type, "HandlerGetFileContents");
+    assertEquals(getType(), "HandlerGetFileContents");
   }
 
   public void testRouteForGetDirContents() {
+    buildRequest("GET /file.txt HTTP/1.1");
     mockFileIO.setIsFileFalse();
     mockFileIO.setIsDirectoryTrue();
-    String type = getType();
-    assertEquals(type, "HandlerGetDirectoryContents");
+    assertEquals(getType(), "HandlerGetDirectoryContents");
   }
 
    public void testRouteForFileNotFound() {
+    buildRequest("GET /file.txt HTTP/1.1");
     mockFileIO.setIsFileFalse();
     mockFileIO.setIsDirectoryFalse();
-    String type = getType();
-    assertEquals(type, "HandlerFileNotFound");
+    assertEquals(getType(), "HandlerFileNotFound");
   }
 
   public void testRouterForPostWithOutBody() {
-    request = new HttpRequest("POST /file.txt HTTP/1.1", new HashMap<String, String>());
-    String type = getType();
-    assertEquals(type, "HandlerUnprocessableEntity");
+    buildRequest("POST /file.txt HTTP/1.1");
+    assertEquals(getType(), "HandlerUnprocessableEntity");
   }
 
   public void testRouterForPostWithOutPath() {
-    request = new HttpRequest("POST / HTTP/1.1", new HashMap<String, String>());
+    buildRequest("POST / HTTP/1.1");
     request.setBody("This".getBytes());
-    String type = getType();
-    assertEquals(type, "HandlerUnprocessableEntity");
+    assertEquals(getType(), "HandlerUnprocessableEntity");
   }
 
   public void testRouterForPost() {
-    request = new HttpRequest("POST /file.txt HTTP/1.1", new HashMap<String, String>());
+    buildRequest("POST /file.txt HTTP/1.1");
     request.setBody("This".getBytes());
-    String type = getType();
-    assertEquals(type, "HandlerPostFileContents");
+    assertEquals(getType(), "HandlerPostFileContents");
+  }
+
+  private void buildRequest(String requestLine) {
+    request = new HttpRequest(requestLine, new HashMap<String, String>());
   }
 
   private String getType() {
