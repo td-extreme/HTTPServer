948b8f29805189ac997a7f670d591adbb5e76104
Tyler Decker <tyler.decker@gmail.com>
2016-07-12 15:48:40 -0500

    added blackbox test script

---
+++ src/blackboxTest/bash/BlackBoxTest.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+TESTPATH=./temp
+EXIT_CODE=0
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+WHITE='\033[0m'
+
+setUp() {
+  mkdir ${TESTPATH}
+  touch ${TESTPATH}/file01.txt
+  ./gradlew run -P args="-d ${TESTPATH}" &
+  SERVER_PID=$!
+  printf "\n\n:Blackbox Tests:\n"
+}
+
+tearDown() {
+  rm -rf ${TESTPATH}
+  kill $SERVER_PID
+}
+
+dispayResults() {
+  if [ $EXIT_CODE -eq 0 ]; then
+    printf "\n\nBLACKBOX TESTS PASSED\n\n"
+  else
+    printf "\n\nBLACKBOX TESTS FAILED\n\n"
+  fi
+}
+
+testGetDirectoryContents() {
+  curl localhost:8080 > ${TESTPATH}/testGet.txt 2>/dev/null
+  echo -n "<!DOCTYPE html><html><body><a href=\"/file01.txt\">file01.txt</a><br><a href=\"/testGet.txt\">testGet.txt</a><br></body></html>" >> ${TESTPATH}/expected.txt
+  printf "\n\n${WHITE}Black Box Test > testGetDirectoryContents"
+  if cmp -s ${TESTPATH}/expected.txt ${TESTPATH}/testGet.txt ; then
+    printf "${GREEN} PASSED ${WHITE}\n"
+  else
+    EXIT_CODE=1
+    printf "${RED} FAILED ${WHITE}\n"
+  fi
+}
+
+setUp
+sleep 5
+testGetDirectoryContents
+tearDown
+dispayResults
+exit $EXIT_CODE
--- test.sh
+++ test.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-
 ./gradlew clean check
+if [ $? -ne 0 ]
+then
+  exit 1
+fi
+
+./src/blackboxTest/bash/BlackBoxTest.sh
