e15ffd85f3739254a7c78fae92a39d4815da9724
Tyler Decker <tyler.decker@gmail.com>
2016-07-19 16:04:26 -0500

    try block only around exception throwing code.

--- src/main/java/request/HttpRequestBuilder.java
+++ src/main/java/request/HttpRequestBuilder.java
@@ -4,25 +4,37 @@ import java.util.HashMap;
 import java.io.IOException;
 
 public class HttpRequestBuilder implements IRequestBuilder {
-  IRequestParser parser;
+  private IRequestParser parser;
 
   public HttpRequestBuilder(IRequestParser parser ) {
     this.parser = parser;
   }
 
   public HttpRequest getNextRequest(IClientSocketIO client) throws InvalidHttpRequestException, BadConnectionException {
+    String rawRequest;
     try {
-      String rawRequest = client.getRawRequestString();
-      HttpRequest request = parser.parseRequest(rawRequest);
-      if (request.contentLength() > 0) {
-        byte[] body = client.getBytes(request.contentLength());
-        request.setBody(body);
-      }
-      return request;
+      rawRequest = client.getRawRequestString();
     }
     catch (IOException e) {
       e.printStackTrace();
       throw new BadConnectionException();
+    }
+    HttpRequest request = parser.parseRequest(rawRequest);
+    addBody(client, request);
+    return request;
+  }
+
+  private void addBody(IClientSocketIO client, HttpRequest request) throws BadConnectionException {
+    byte[] body;
+    if (request.contentLength() > 0) {
+      try {
+        body = client.getBytes(request.contentLength());
+      }
+      catch (IOException e) {
+        e.printStackTrace();
+        throw new BadConnectionException();
+      }
+      request.setBody(body);
     }
   }
 }
