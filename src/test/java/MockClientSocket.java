9b3801af3567289f022175f82dfca5678c636827
Tyler Decker <tyler.decker@gmail.com>
2016-07-21 08:53:20 -0500

    Added Mock used for MultiThreadTest

---
+++ src/test/java/MockClientSocket.java
@@ -0,0 +1,40 @@
+package com.td.Mocks;
+import com.td.HttpServer.*;
+
+public class MockClientSocket implements IClientSocketIO {
+  private long closedTime;
+  private int sleepTime;
+
+  public MockClientSocket(int sleepTime) {
+    this.sleepTime = sleepTime;
+  }
+
+  public void sleep() {
+    try {
+      Thread.sleep(sleepTime);
+    }
+    catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public Long getClosedTime() {
+    return closedTime;
+  }
+
+  public void closeClientConnection() {
+    closedTime = System.currentTimeMillis();
+  }
+
+  public void openClientConnection() throws BadConnectionException {}
+
+  public IClientSocketInput clientSocketInput() {
+    return new MockClientSocketInput("GET / HTTP/1.1", new byte[0]);
+  }
+
+  public IClientSocketOutput clientSocketOutput() {
+    return new MockClientSocketOutput();
+  }
+}
+
+
