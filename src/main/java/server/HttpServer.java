3d1f162fb2232470f15addfbf3e7d8067da3a54f
Tyler Decker <tyler.decker@gmail.com>
2016-07-26 14:24:49 -0500

    An instance of HttpServer can now be created.

--- src/main/java/server/HttpServer.java
+++ src/main/java/server/HttpServer.java
@@ -1,30 +1,45 @@
 package com.td.HttpServer;
 import java.io.IOException;
 import java.net.ServerSocket;
+
 public class HttpServer{
 
-  private static ArgumentParser arguments;
+  private ServerSocket serverSocket;
+  private HttpServerRunner httpServerRunner;
+  private CustomHandlerRouter customHandlerRouter;
+  private FileIO fileIO;
 
-  public static void main(String[] args) {
-    arguments = new ArgumentParser(args);
-    ServerSocket serverSocket;
+  public HttpServer(int portNumber, String directory) {
     try {
-      serverSocket = new ServerSocket(arguments.getPortNumber());
+      serverSocket = new ServerSocket(portNumber);
     }
     catch (IOException e) {
-      System.out.println("\nPort number " + arguments.getPortNumber() + " can not be opened. It may be in use by another application.\n");
+      System.out.println("\nPort number " + portNumber + " can not be opened. It may be in use by another application.\n");
       return;
     }
     HttpRequestParser httpRequestParser = new HttpRequestParser();
-    FileIO fileIO = new FileIO(arguments.getDirectory());
+    fileIO = new FileIO(directory);
     DefaultHandlerRouter defaultHandlerRouter = new DefaultHandlerRouter(fileIO);
-    CustomHandlerRouter customHandlerRouter = new CustomHandlerRouter(defaultHandlerRouter);
+    customHandlerRouter = new CustomHandlerRouter(defaultHandlerRouter);
     HttpRequestBuilder httpRequestBuilder = new HttpRequestBuilder(httpRequestParser);
     HttpResponseWriter httpResponseWriter = new HttpResponseWriter();
     ConnectionProcessRunnerMultiThread connectionProcessRunner = new ConnectionProcessRunnerMultiThread();
-    HttpServerRunner httpServerRunner = new HttpServerRunner(serverSocket, connectionProcessRunner, customHandlerRouter, httpRequestBuilder, httpResponseWriter);
+    httpServerRunner = new HttpServerRunner(serverSocket, connectionProcessRunner, customHandlerRouter, httpRequestBuilder, httpResponseWriter);
+  }
+
+  public void run() {
+    httpServerRunner.run();
     System.out.println("HTTP Server running on localhost port " + serverSocket.getLocalPort() +"!");
     System.out.println("Using directory : " + fileIO.workingDirectory());
-    httpServerRunner.run();
- }
+  }
+
+  public void addRoute(String method, String path, IHandler handler) {
+    customHandlerRouter.addRoute(method, path, handler);
+  }
+
+  public static void main(String[] args) {
+    ArgumentParser arguments = new ArgumentParser(args);
+    HttpServer httpServer = new HttpServer(arguments.getPortNumber(), arguments.getDirectory());
+    httpServer.run();
+  }
 }
