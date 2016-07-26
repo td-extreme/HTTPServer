c92665e7101ae91cb44f16b1f5c5e1e3f1fdff0c
Tyler Decker <tyler.decker@gmail.com>
2016-07-26 11:43:24 -0500

    Fixed spacing.

--- src/main/java/httpConnectionProcessing/HttpConnectionToProcessThread.java
+++ src/main/java/httpConnectionProcessing/HttpConnectionToProcessThread.java
@@ -2,8 +2,9 @@ package com.td.HttpServer;
 
 public class HttpConnectionToProcessThread implements Runnable {
   private HttpConnectionToProcess httpConnectionToProcess;
+
   public HttpConnectionToProcessThread(HttpConnectionToProcess httpConnectionToProcess) {
-  this.httpConnectionToProcess = httpConnectionToProcess;
+    this.httpConnectionToProcess = httpConnectionToProcess;
   }
   public void run() {
     httpConnectionToProcess.execute();
